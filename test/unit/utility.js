f95001ffec83524391399a6c0c7ca57d9ab8987b
Andrew Sliwinski <andrewsliwinski@acm.org>
2018-12-17 20:12:21 -0500

    Clean-up utility module / tests

---
+++ test/unit/utility.js
@@ -0,0 +1,19 @@
+const test = require('tap').test;
+const utility = require('../../lib/utility');
+
+test('spec', t => {
+    t.type(utility, 'function');
+    t.type(utility.frequency, 'function');
+    t.end();
+});
+
+test('frequency', t => {
+    const input = ['foo', 'foo', 'foo', 'bar', 'bar', 'baz'];
+    const result = utility.frequency(input);
+    t.deepEqual(result, {
+        foo: 3,
+        bar: 2,
+        baz: 1
+    });
+    t.end();
+});
